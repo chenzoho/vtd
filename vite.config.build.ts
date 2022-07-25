ae0422f535f2c312a695393aa85541a82e2835d7
陈泽洪 <447188858@qq.com>
2022-07-25 08:59:12 +0800

    init

---
+++ vite.config.build.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    dts({
+      outputDir: ["dist/types"],
+      exclude: ["src"],
+      logDiagnostics: true,
+    }),
+    vue(),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.ts"),
+      name: "vtd",
+      fileName: (format) => `vtd.${format}.js`,
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ["vue"],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
