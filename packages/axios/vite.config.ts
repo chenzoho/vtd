b732b3750020287ce2bee0fb704dbb9b27edc15b
陈泽洪 <447188858@qq.com>
2022-07-27 09:53:24 +0800

    axios

--- packages/axios/vite.config.ts
+++ packages/axios/vite.config.ts
@@ -19,10 +19,18 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, "src/main.ts"),
-			name: "vtd",
-			fileName: format => `vtd.${format}.js`
+			name: "vtd.axios",
+			fileName: format => `vtd.axios.${format}.js`
 		},
 		sourcemap: true,
+		// minify: "terser",
+		// terserOptions: {
+		// 	compress: {
+		// 		drop_console: true,
+		// 		drop_debugger: true
+		// 	},
+		// 	keep_classnames: true
+		// },
 		rollupOptions: {
 			// 确保外部化处理那些你不想打包进库的依赖
 			external: ["vue", "axios", "qs", "mitt"],
